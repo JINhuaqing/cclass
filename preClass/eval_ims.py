2d0d9eafba3732f28cb8d3108e2914ae424e12f8
JINhuaqing <kevinjin0423@gmail.com>
2018-09-01 21:23:59 -0400

    finish the evaluate code

--- preClass/eval_ims.py
+++ preClass/eval_ims.py
@@ -4,11 +4,23 @@ import torch.nn as nn
 from torchvision.models import resnet18
 from torchvision import transforms as tsfms
 from PIL import Image
+from pathlib import Path
+import numpy as np
+
+
+def evalt(imlist, net, tsfm):
+    imlist = [tsfm(im) for im in imlist]
+    inputs = torch.stack(imlist)
+    if is_cuda: inputs = inputs.cuda()
+    outputs = net(inputs)
+    return outputs
+
 
 # get the labels
 with open('./documents/labels.txt') as f:
     labels = f.readlines()
 labels = [i.strip() for i in labels]
+labels = np.array(labels)
 
 # mean, std of imagenet images
 imagenetmean = [0.485, 0.456, 0.406]
@@ -19,6 +31,8 @@ tsfm = tsfms.Compose([tsfms.Resize([224, 224]), tsfms.ToTensor(), tsfms.Normaliz
 
 # some parameters
 is_cuda = torch.cuda.is_available()
+numIms = 10
+root = Path('/home/feijiang/datasets/images')
 
 # get the net
 net = resnet18(True)
@@ -26,15 +40,24 @@ net.eval()
 if is_cuda:
     net = net.cuda()
 
-# get the image
-im = Image.open('./documents/1.jpg')
-
 # run the net
-imts = tsfm(im)
-imts = imts.unsqueeze(0)
-if is_cuda:
-    imts = imts.cuda()
-out = net(imts)
-outprob = nn.Softmax(dim=1)(out)
-cls = out.cpu().detach().numpy().argmax()
-print(labels[cls], cls)
+imnamelist = []
+imlist = []
+flag = 1
+f = open('./savedoc/test.txt', 'w')
+for p1 in root.iterdir():
+    for p2 in p1.iterdir():
+        im = Image.open(p2)
+        if len(im.getbands()) == 3:
+            imlist.append(im)
+            imnamelist.append(str(p2))
+        if len(imlist) == numIms:
+            outs = evalt(imlist, net, tsfm)
+            cls = outs.cpu().detach().numpy().argmax(axis=1)
+            print(flag, *(labels[cls]), sep='---')
+            fileout = [ imnamelist[i]+' '+labels[cls][i]+'\n' for i in range(len(imlist))]
+            f.writelines(fileout)
+            imlist = []
+            imnamelist = []
+            flag += 1
+f.close()
