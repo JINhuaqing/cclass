550fc2da0f5d62be607689fb1103974cea1b4990
JINhuaqing <kevinjin0423@gmail.com>
2018-09-04 10:31:41 -0400

    update the data_process.py

--- preClass/data_process.py
+++ preClass/data_process.py
@@ -7,24 +7,32 @@ from PIL import Image
 
 root = Path('/home/feijiang/datasets/images')
 
-rgb = [] 
+rgbmean = [] 
+rgbstd = []
 outflag = 0
 for p1 in root.iterdir():
     lst = list(p1.iterdir())[:20]
     np.random.shuffle(lst)
     for p2 in lst: 
         try:
-            img = np.array(Image.open(p2))
-            add3 = np.mean(img, axis=(0, 1))
-        except IOError:
-            add3 = np.array([0, 0, 0])
+            with Image.open(p2) as f:
+                img = np.array(f)
+            mean = np.mean(img, axis=(0, 1))
+            std = np.std(img, axis=(0, 1))
+        except ValueError:
+            mean = np.array([0, 0, 0])
+            std = np.array([0, 0, 0])
         except IndexError:
-            add3 = np.array([0, 0, 0])
-        if add3.sum() != 0:
-            rgb.append(add3) 
+            mean = np.array([0, 0, 0])
+            std = np.array([0, 0, 0])
+        if mean.sum() != 0:
+            rgbmean.append(mean)
+            rgbstd.append(std)
             outflag += 1
         print(outflag)
-rgb = np.array(rgb)
-channelmean = np.mean(rgb, axis=0)
-with open('./savedoc/mean.txt', 'w') as f:
-    f.write(str(channelmean))
+channelmean = np.mean(rgbmean, axis=0)
+channelstd = np.std(rgbstd, axis=0)
+with open('./savedoc/mean.pkl', 'wb') as fmean:
+    pickle.dump(channelmean, fmean)
+with open('./savedoc/std.pkl', 'wb') as fstd:
+    pickle.dump(channelstd, fstd)
